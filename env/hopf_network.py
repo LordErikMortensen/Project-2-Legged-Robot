f3f91d0d8dc9af34ae088397be6e362e3fd7d475
Diego Celio <diego.celio@epfl.ch>
2023-11-28 14:18:46 +0100

    hopf_network

--- env/hopf_network.py
+++ env/hopf_network.py
@@ -102,11 +102,28 @@ class HopfNetwork():
   def _set_gait(self,gait):
     """ For coupling oscillators in phase space. 
     [TODO] update all coupling matrices
+    Foot Order is FR, FL, RR, RL
     """
-    self.PHI_trot = np.zeros((4,4))
-    self.PHI_walk = np.zeros((4,4))
-    self.PHI_bound = np.zeros((4,4))
-    self.PHI_pace = np.zeros((4,4))
+    d = 0.5*np.pi
+    self.PHI_trot = np.array([[  0, -2*d, -2*d,   0],
+                              [2*d,    0,    0, 2*d],
+                              [2*d,    0,    0, 2*d],
+                              [  0, -2*d, -2*d,   0]])
+    
+    self.PHI_walk = np.array([[  0, -2*d,   -d,   d],
+                              [2*d,    0,    d, 3*d],
+                              [  d,   -d,    0, 2*d],
+                              [ -d, -3*d, -2*d,   0]])
+    
+    self.PHI_bound= np.array([[  0,   0, -2*d, -2*d],
+                              [  0,   0, -2*d, -2*d],
+                              [2*d, 2*d,    0,    0],
+                              [2*d, 2*d,    0,    0]])
+    
+    self.PHI_pace = np.array([[   0, -2*d,   0, -2*d],
+                              [ 2*d,    0, 2*d,    0],
+                              [   0, -2*d,   0, -2*d],
+                              [ 2*d,    0, 2*d,   0]])
 
     if gait == "TROT":
       self.PHI = self.PHI_trot
@@ -130,14 +147,17 @@ class HopfNetwork():
       self._integrate_hopf_equations_rl()
     
     # map CPG variables to Cartesian foot xz positions (Equations 8, 9) 
-    thetas = self.get_theta()
-    x = -self.des_step_len*self.get_r()*np.cos(thetas)# [TODO]
-    z = np.zeros(4) # [TODO]
-    for i in range(len(thetas)):
-        if np.sin(thetas[i])>0:
-            z[i] = -self._robot_height+self._ground_clearance*np.sin(thetas[i])
-        else:
-            z[i] = -self._robot_height+self._ground_penetration*np.sin(thetas[i])
+    X = self.X.copy()
+    x = np.zeros(4)
+    z = np.zeros(4)
+
+    for i in range(4):
+      r, theta = X[:,i]
+      x[i] = self._des_step_len * r * np.cos(theta) # [TODO]
+      if np.sin(theta) > 0:
+        z[i] = -self._robot_height + self._ground_clearance * np.sin(theta) # [TODO] 
+      else:
+        z[i] = -self._robot_height + self._ground_penetration * np.sin(theta) # [TODO] 
 
     # scale x by step length
     if not self.use_RL:
@@ -160,21 +180,24 @@ class HopfNetwork():
     # loop through each leg's oscillator
     for i in range(4):
       # get r_i, theta_i from X
-      r, theta = 0, 0 # [TODO]
+      r, theta = X[:,i] # [TODO]
       # compute r_dot (Equation 6)
-      r_dot = 0 # [TODO]
+      r_dot = self._alpha * (self._mu - r**2) * r # [TODO]
       # determine whether oscillator i is in swing or stance phase to set natural frequency omega_swing or omega_stance (see Section 3)
-      theta_dot = 0 # [TODO]
+      mod_theta = theta % np.pi
+      theta_dot = theta_dot = self._omega_swing  if np.sin(theta) > 0 else self._omega_stance # [TODO/ NOTOURS]
 
       # loop through other oscillators to add coupling (Equation 7)
       if self._couple:
-        theta_dot += 0 # [TODO]
+        for j in range(4):
+          theta_dot += X[0,j]*self._coupling_strength*np.sin(X[1,j]-theta-self.PHI[i,j]) #[TODO/NOTOURS]
 
       # set X_dot[:,i]
       X_dot[:,i] = [r_dot, theta_dot]
 
     # integrate 
-    self.X = np.zeros((2,4)) # [TODO]
+    self.X[0,:] = np.array([np.add(self.X[0,:],X_dot_prev[0,:]*self._dt)])
+    self.X[1,:] = np.array([np.add(self.X[1,:],X_dot_prev[1,:]*self._dt)]) # [TODO/NOTOURS]
     self.X_dot = X_dot
     # mod phase variables to keep between 0 and 2pi
     self.X[1,:] = self.X[1,:] % (2*np.pi)
@@ -218,9 +241,9 @@ class HopfNetwork():
       # get r_i, theta_i from X
       r, theta = X[:,i]
       # amplitude (use mu from RL, i.e. self._mu_rl[i])
-      r_dot = 0  # [TODO]
+      r_dot = self.alpha * (self._mu[i] - r^2) * r  # [TODO]
       # phase (use omega from RL, i.e. self._omega_rl[i])
-      theta_dot = 0 # [TODO]
+      theta_dot = self._omega_rl[i] *  1# [TODO]
 
       X_dot[:,i] = [r_dot, theta_dot]
 
